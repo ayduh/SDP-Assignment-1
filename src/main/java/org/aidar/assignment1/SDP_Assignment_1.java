ef676849a9aace266845c445bd3dd55c7b9cac17
Aidar <ajdrmrt@gmail.com>
2026-09-10 11:46:44 +0500

    Initial commit

---
+++ src/main/java/org/aidar/assignment1/SDP_Assignment_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.aidar.assignment1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class SDP_Assignment_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
