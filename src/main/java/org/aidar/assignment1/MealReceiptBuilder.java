2d8b4bdd0ba04cdd47b9498224be4ef4d829efbd
Aidar <ajdrmrt@gmail.com>
2026-09-11 17:27:00 +0500

    Add MealReceiptBuilder class

---
+++ src/main/java/org/aidar/assignment1/MealReceiptBuilder.java
@@ -0,0 +1,20 @@
+package org.aidar.assignment1;
+
+public class MealReceiptBuilder implements MealBuilder{
+    private final StringBuilder sb = new StringBuilder("- Order Receipt -\n");
+
+    public MealReceiptBuilder setMainDish(String main){
+        sb.append("Main Dish: ").append(main).append("\n");
+        return this;
+    }
+
+    public MealReceiptBuilder setSideDish(String side){
+        sb.append("Side Dish: ").append(side).append("\n");
+        return this;
+    }
+
+    public MealReceiptBuilder setDrink(String drink){
+        sb.append("Drink Dish: ").append(drink).append("\n");
+        return this;
+    }
+}
