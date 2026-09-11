815b104446d49a762336285e6ec313cfb204e943
Aidar <ajdrmrt@gmail.com>
2026-09-12 00:08:41 +0500

    Made two different builder instances for each configuration to output the info properly

--- src/main/java/org/aidar/assignment1/Client.java
+++ src/main/java/org/aidar/assignment1/Client.java
@@ -3,14 +3,27 @@ package org.aidar.assignment1;
 public class Client{
     public static void main(String[] args) {
         MealDirector dr = new MealDirector();
-        
-        MealObjectBuilder ob = new MealObjectBuilder();
-        dr.makeMeatMeal(ob);
-        Meal meatMeal = ob.getResult();
 
-        MealReceiptBuilder rb = new MealReceiptBuilder();
-        dr.makeMeatMeal(rb);
-        String mealReceipt = rb.getResult();
-        System.out.println(mealReceipt);
+        // Meat Meal
+        MealObjectBuilder meatOB = new MealObjectBuilder();
+        dr.makeMeatMeal(meatOB);
+        Meal meatMeal = meatOB.getResult();
+        System.out.println("= Meat Meal Object =:\n" + meatMeal + "\n");
+
+        MealReceiptBuilder meatRB = new MealReceiptBuilder();
+        dr.makeMeatMeal(meatRB);
+        String meatReceipt = meatRB.getResult();
+        System.out.println(meatReceipt + "\n");
+
+        // Vegan Meal
+        MealObjectBuilder veganOB = new MealObjectBuilder();
+        dr.makeVeganMeal(veganOB);
+        Meal veganMeal = veganOB.getResult();
+        System.out.println("= Vegan Meal Object =:\n" + veganMeal + "\n");
+
+        MealReceiptBuilder veganRB = new MealReceiptBuilder();
+        dr.makeVeganMeal(veganRB);
+        String veganReceipt = veganRB.getResult();
+        System.out.println(veganReceipt + "\n");
     }
 }
